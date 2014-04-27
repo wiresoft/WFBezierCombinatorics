76f4670e49452a76708d7e24aad2d283d392f5c4
Noah Desch <deschnl@me.com>
2014-04-27 01:06:18 -0400

    Merged in bug fixes

--- NSBezierPath+WFBezierCombinatorics.h
+++ NSBezierPath+WFBezierCombinatorics.h
@@ -27,9 +27,4 @@
  */
 - (NSBezierPath *)WFSubtractPath:(NSBezierPath *)path;
 
-/** Returns a new NSBezierPath with all edges inset by a specified amount.
- @param delta The amount to inset each path edge.
- */
-- (NSBezierPath *)WFInsetPath:(CGFloat)delta;
-
 @end
